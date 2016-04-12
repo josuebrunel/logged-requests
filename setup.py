166c09991bfcca2f5eaeaf4dbc6ef3e6f9d6815f
Josue Kouka <jkouka@entrouvert.com>
2016-04-12 18:12:51 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup, find_packages
+
+__author__ = 'Josue Kouka'
+__email__ = 'josuebrunel@gmail.com'
+__version__ = '0.0.1'
+
+#requirements.txt
+with open('requirements.txt') as f:
+  required = f.read().splitlines()
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+  name = "logged_requests",
+  version = __version__,
+  description = "",
+  long_description = read("README.rst"),
+  author = __author__,
+  author_email = __email__,
+  url = "https://github.com/josuebrunel/logged-requests",
+  download_url = "https://github.com/josuebrunel/logged-requests/archive/{0}.tar.gz".format(__version__),
+  keywords = ['requests', 'logging', 'log', 'logger', 'logged requests'],
+  packages = find_packages(),
+  classifiers = [
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Development Status :: 5 - Production/Stable',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License'
+  ],
+  platforms=['Any'],
+  license='MIT',
+  install_requires = required
+)
